bac4349b2544ac750ed159b90d0f3524ff1c10e3
praveen <cbsrirangapraveen.2004@gmail.com>
2023-06-25 14:15:38 +0530

    resolved last model error

--- chatapp/manage.py
+++ chatapp/manage.py
@@ -2,23 +2,20 @@ from typing import Any
 from django.contrib.auth.base_user import BaseUserManager
 
 class CustomUserManager(BaseUserManager):
-    def _create_user(self,userid,password,email,name,**extra_fields):
-        if not username:
+    def _create_user(self,userid,password,email,**extra_fields):
+        if not userid:
             raise ValueError("User name Required")
-        
-        extra_fields['email']=self.normalize_email(extra_fields['email'])
-        user=self.model(username=username,email=email,name=name,**extra_fields)
+        email = extra_fields.get('email')
+        email=self.normalize_email(email)
+        user=self.model(userid=userid,email=email,**extra_fields)
         user.set_password(password)
 
         user.save(using=self.db)
 
         return user
     
-    def create_user(self,userid,password,email,name,**extra_fields):
-        extra_fields.setdefault('is_staff',True)
-        extra_fields.setdefault('is_active',True)
-        extra_fields.setdefault('is_superuser',False)
-        return self._create_user(userid,password,email,name,**extra_fields)
+    def create_user(self,userid,password,email,**extra_fields):
+        return self._create_user(userid,password,email,**extra_fields)
     
     def create_superuser(self,userid,password=None,**extra_fields):
         extra_fields.setdefault('is_staff',True)

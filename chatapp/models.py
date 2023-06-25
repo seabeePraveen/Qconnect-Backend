e25c3dfbce5fd5605938aff05dfdd949c894f0da
praveen <cbsrirangapraveen.2004@gmail.com>
2023-06-25 14:39:41 +0530

    create user required fields solved

--- chatapp/models.py
+++ chatapp/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import AbstractBaseUser
 from .manage import CustomUserManager
 
 class CustomUser(AbstractBaseUser):
-    phone_number=models.CharField(max_length=10)
-    user_profile_image=models.ImageField(upload_to="profile")
+    phone_number=models.CharField(max_length=10,null=True,blank=True)
+    user_profile_image=models.ImageField(upload_to="profile",null=True)
     email=models.EmailField(unique=True)
     name = models.CharField(max_length=100,null=True,blank=True)
     objects = CustomUserManager()

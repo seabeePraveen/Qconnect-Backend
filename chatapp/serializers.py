ec963362530b3a9c7edc26c2a41c64fae179d1da
praveen <cbsrirangapraveen.2004@gmail.com>
2023-06-25 15:53:10 +0530

    modified register endpoint and working succesfully

--- chatapp/serializers.py
+++ chatapp/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.validators import ValidationError
 from .models import *
 
 
@@ -6,6 +7,15 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['userid','password','email','phone_number']
+
+    def validate(self,attrs):
+        email_exists = CustomUser.objects.filter(email=attrs['email']).exists()
+
+        if email_exists:
+            raise ValidationError("Email has already in Use")
+
+        return super().validate(attrs)
+        
 
     
 
--- chatapp/views.py
+++ chatapp/views.py
@@ -1,19 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from rest_framework.views import APIView
 from .serializers import *
 from .models import *
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-from rest_framework import generics
-from .models import CustomUser
-from .serializers import UserSerializer
+from rest_framework import generics,status
+from rest_framework.request import Request
 # Create your views here.
 
 def home(request):
     return render(request,"register.html")
 
+
+
+
 class CustomUserCreateView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
 
+    def post(self,request:Request):
+        data = request.data
+        serializer = self.serializer_class(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            response = {
+                "message":"User Created Succesfully",
+                "data": serializer.data
+            }
+            return Response(data=response,status=status.HTTP_201_CREATED)
+
+        response = {
+            "message":serializer.errors,
+            "data": serializer.data
+        }
+        
+        return Response(data=response,status=status.HTTP_400_BAD_REQUEST)

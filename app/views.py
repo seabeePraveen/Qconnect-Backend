bad428b536c219ac0ab3fe4ca840c13b9c58695a
praveen <cbsrirangapraveen.2004@gmail.com>
2023-06-26 13:00:04 +0530

    errors giving in register view done

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics,status
 from rest_framework.response import Response
 from rest_framework.request import Request
 import io
+from .models import User
 from rest_framework.parsers import JSONParser
 # Create your views here.
 
@@ -12,9 +13,28 @@ class SignUpView(generics.GenericAPIView):
 
     def post(self,request:Request):
         data = request.data
-        serializer = self.serializer_class(data=data)
+        username = data['username']
+        email = data['email']
+        print(username)
+        print(email)
+        username_exits = User.objects.filter(username=username).exists()
+        if username_exits:
+            response = {
+                "username":"UserName Already exits"
+            }
+            return Response(data=response,status=status.HTTP_400_BAD_REQUEST)
 
-        if serializer.is_valid(raise_exception=True):
+        email_exits = User.objects.filter(email=email).exists()
+        if email_exits:
+            response = {
+                "email":"Email Already exits"
+            }
+            return Response(data=response,status=status.HTTP_400_BAD_REQUEST)
+
+
+        serializer = self.serializer_class(data=data)
+        try:
+            serializer.is_valid(raise_exception=True)
             serializer.save()
 
             response = {
@@ -23,9 +43,9 @@ class SignUpView(generics.GenericAPIView):
             }
 
             return Response(data=response,status=status.HTTP_200_OK)
-        else:
+        except :
             response = {
-                "message":serializer.errors
+                "error":"Error occured"
             }
             return Response(data=response,status=status.HTTP_400_BAD_REQUEST)
 

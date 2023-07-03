4feed542b71d165c70986b491836d48fbf271635
Praveen CB <88821454+seabeePraveen@users.noreply.github.com>
2023-07-03 19:36:57 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -118,12 +118,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = '/media/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'media/'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
+STATIC_ROOT = os.path.join(BASE_DIR,'mediafiles_build','media')
 
 
 # Default primary key field type
